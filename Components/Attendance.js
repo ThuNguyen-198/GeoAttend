ca7d35b84c762d3518dfdf92a9b4fb876cd2d1fc
Thu Nguyen <66782390+ThuNguyen-198@users.noreply.github.com>
2024-10-16 15:01:21 -0400

    TN : add course details page

--- Components/Attendance.js
+++ Components/Attendance.js
@@ -1,4 +1,87 @@
-import { Text } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default function Attendance() {
-  return <Text>Attendance</Text>;
+  const courses = [
+    { courseName: "Advanced Database", numOfClass: 4, attended: 3 },
+    { courseName: "Capstone Project", numOfClass: 8, attended: 8 },
+    { courseName: "Advanced Algorithm", numOfClass: 8, attended: 4 },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Attendance</Text>
+      </View>
+
+      <FlatList
+        data={courses}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={[styles.card, styles.row]}>
+            <Text style={styles.item}>{item.courseName}</Text>
+            <View>
+              <Text></Text>
+              <MaterialCommunityIcons
+                name="chevron-down"
+                style={styles.dropdownIcon}
+              />
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+    paddingHorizontal: 14,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    color: "#000",
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    elevation: 5,
+    borderWidth: 0.3,
+    borderColor: "#A4A4A4",
+  },
+  item: {
+    fontSize: 18,
+    color: "#013976",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "left",
+    gap: 10,
+    paddingVertical: 14,
+  },
+  dropdownIcon: {
+    fontSize: 30,
+    color: "#424242",
+  },
+});
--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    padding: 16,
+    padding: 22,
     backgroundColor: "#fff",
   },
   title: {
